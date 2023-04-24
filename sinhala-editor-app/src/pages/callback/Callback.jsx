5fef5fb5c488377a0773f00a4b8afc7893871884
Mohamed Rashad <bmorashad@gmail.com>
2023-04-24 16:28:04 +0530

    Handle callback error on login

--- sinhala-editor-app/src/pages/callback/Callback.jsx
+++ sinhala-editor-app/src/pages/callback/Callback.jsx
@@ -1,7 +1,18 @@
 import {Flex, LoadingOverlay} from "@mantine/core";
-import React from "react";
+import React, {useEffect} from "react";
+import {useAuth0} from "@auth0/auth0-react";
 
 export default function Callback() {
+  const { logout } = useAuth0()
+      useEffect(() => {
+    const urlQueryParams = new URLSearchParams(
+        window.location.search
+    );
+    if (urlQueryParams.has("error")) {
+      console.log("Error")
+      logout()
+    }
+  }, []);
 
   return (<Flex sx={{width: "100vw"}} >
     <LoadingOverlay
