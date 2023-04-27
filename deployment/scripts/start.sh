a462ab22fb007ba190d462a89f5da9f07d7d842d
Mohamed Rashad <bmorashad@gmail.com>
2023-04-27 20:52:54 +0530

    Improve start script

--- deployment/scripts/start.sh
+++ deployment/scripts/start.sh
@@ -2,10 +2,17 @@
 
 echo "Remove old containers and images"
 docker compose down
-backend=$(docker compose config --images backend)
-frontend=$(docker compose config --images backend)
-docker image rm "$backend:*"
-docker image rm "$frontend:*"
+backend=$(docker images -q sinhala-text-genie-backend | uniq)
+frontend=$(docker images -q sinhala-text-genie-frontend | uniq)
+
+if [[ -n "$backend" ]]; then
+  echo "Removing backend image with the id $backend"
+  docker image rm -f $backend
+fi
+if [[ -n "$frontend" ]]; then
+  echo "Removing frontend image with the id $frontend"
+  docker image rm -f $frontend
+fi
 
 echo "Build and run the new ones"
 docker compose build
