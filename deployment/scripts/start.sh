5dc3cee36754299e26ee1ca673c347d18aed4325
Mohamed Rashad <bmorashad@gmail.com>
2023-04-27 18:24:22 +0530

    Fix start script

--- deployment/scripts/start.sh
+++ deployment/scripts/start.sh
@@ -1,14 +1,12 @@
 #!bin/bash
 
-git pull
-
-# Remove old containers and images
+echo "Remove old containers and images"
 docker compose down
-backend=($docker compose config --images backend)
-frontend=($docker compose config --images backend)
+backend=$(docker compose config --images backend)
+frontend=$(docker compose config --images backend)
 docker image rm "$backend:*"
 docker image rm "$frontend:*"
 
-# Build and run the new ones
+echo "Build and run the new ones"
 docker compose build
 docker compose up
