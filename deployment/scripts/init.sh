6da59814b3a2e65b66580e69d84e6867a5a76751
Mohamed Rashad <bmorashad@gmail.com>
2023-04-28 22:21:44 +0530

    Expect server to have the repository cloned & do the pulling only

--- deployment/scripts/init.sh
+++ deployment/scripts/init.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 REPO_NAME=sinhala-text-genie
-if [ -d "$REPO_NAME" ]; then
-  echo "Repository already exists, skipping clone & pulling"
-  cd $REPO_NAME
-  git pull
-else
-  echo "Cloning repository"
-  git clone https://github.com/bmorashad/sinhala-text-genie.git
-fi
+echo "Pulling latest updates..."
+cd $REPO_NAME
+git pull
